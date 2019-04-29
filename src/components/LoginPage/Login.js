0d2a6a780b43b5add4cfc1c760644d93e75d29c4
Matt Groberg <grobergmg@gmail.com>
2019-04-29 13:35:28 -0700

    add login hero-banner

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import heroBkg from '../../img/hero-background.jpg';
+import logo from '../../img/netflix-logo.png';
 
 function Login(){
+	const heroBanner={
+		backgroundImage:`url(${heroBkg})`,
+		height:'550px',
+		textAlign:'center'
+	}
+	const nav={
+		display: 'flex',
+		justifyContent:'space-between',
+		padding:'10px 20px'
+	}
+	const callToAction={
+		width:'60vw',
+		margin:'0 auto'
+	}
 	return (
 		<div>
-			Login Works
+			<div style={heroBanner}>
+				<div style={nav}>
+					<img className='logo' src={logo}/>
+					<a className='button' LinkTo='/browse'>Sign In</a>
+				</div>
+				<div style={callToAction}>
+					<h1>Unlimited movies, TV, shows, and more.</h1>
+					<h2>Watch anywhere. Cancel anytime.</h2>
+					<a className='button'>WATCH FREE FOR 30 DAYS</a>
+				</div>
+			</div>
 		</div>
 		)
 }
