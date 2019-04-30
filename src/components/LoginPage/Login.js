10aaffe3e048b712f2dca834ae9dcc546ca703ff
Matt Groberg <grobergmg@gmail.com>
2019-04-30 13:38:49 -0700

    change to alternate herobanner

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import heroBkg from '../../img/hero-background.jpg';
+import heroSM from '../../img/login/hero_bkg_small.jpg';
+import heroMD from '../../img/login/hero_bkg_medium.jpg';
+import heroLG from '../../img/login/hero_bkg_large.jpg';
 import logo from '../../img/netflix-logo.png';
 
 function Login(){
 	const heroBanner={
-		backgroundImage:`url(${heroBkg})`,
-		height:'550px',
+		position:'relative',
+		backgroundImage:`url(${heroMD})`,
+		backgroundSize:'cover',
+		height:'400px',
 		textAlign:'center'
 	}
 	const nav={
@@ -14,24 +18,38 @@ function Login(){
 		padding:'10px 20px'
 	}
 	const callToAction={
-		width:'60vw',
-		margin:'0 auto'
+		margin:'0 auto',
+	}
+	const darkScreen={
+		position:'absolute',
+		top:'0',
+		left:'0',
+		right:'0',
+		bottom:'0',
+		backgroundImage:'linear-gradient(0deg,rgba(0,0,0,0) 50%,rgba(0,0,0,.5) 100%),radial-gradient(50% 120%,rgba(0,0,0,0) 50%,rgba(0,0,0,.7) 100%)'
 	}
 	return (
 		<div>
 			<div style={heroBanner}>
-				<div style={nav}>
-					<img className='logo' src={logo}/>
+				<div style={darkScreen}>
+					<div style={nav}>
+					<a className='logo'>
+						<svg viewBox="0 0 111 30" class="svg-icon svg-icon-netflix-logo" focusable="true"><title></title><g id="netflix-logo"><path d="M105.062 14.28L111 30c-1.75-.25-3.499-.563-5.28-.845l-3.345-8.686-3.437 7.969c-1.687-.282-3.344-.376-5.031-.595l6.031-13.75L94.468 0h5.063l3.062 7.874L105.875 0h5.124l-5.937 14.28zM90.47 0h-4.594v27.25c1.5.094 3.062.156 4.594.343V0zm-8.563 26.937c-4.187-.281-8.375-.53-12.656-.625V0h4.687v21.875c2.688.062 5.375.28 7.969.405v4.657zM64.25 10.657v4.687h-6.406V26H53.22V0h13.125v4.687h-8.5v5.97h6.406zm-18.906-5.97V26.25c-1.563 0-3.156 0-4.688.062V4.687h-4.844V0h14.406v4.687h-4.874zM30.75 15.593c-2.062 0-4.5 0-6.25.095v6.968c2.75-.188 5.5-.406 8.281-.5v4.5l-12.968 1.032V0H32.78v4.687H24.5V11c1.813 0 4.594-.094 6.25-.094v4.688zM4.78 12.968v16.375C3.094 29.531 1.593 29.75 0 30V0h4.469l6.093 17.032V0h4.688v28.062c-1.656.282-3.344.376-5.125.625L4.78 12.968z" id="Fill-14"></path></g></svg>
+					</a>
 					<a className='button' LinkTo='/browse'>Sign In</a>
+					</div>
+					<div style={callToAction}>
+						<h1>See What's Next.</h1>
+						<h3 className='caps'>Watch anywhere. Cancel anytime.</h3>
+						<a className='button caps'>watch free for 30 days</a>
+					</div>
 				</div>
-				<div style={callToAction}>
-					<h1>Unlimited movies, TV, shows, and more.</h1>
-					<h2>Watch anywhere. Cancel anytime.</h2>
-					<a className='button'>WATCH FREE FOR 30 DAYS</a>
-				</div>
+				
 			</div>
+
 		</div>
 		)
 }
 
 export default Login;
+
