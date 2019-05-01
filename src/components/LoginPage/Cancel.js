67610ba41040a676b53bed38befe5aef6dede3f8
Matt Groberg <grobergmg@gmail.com>
2019-05-01 07:34:43 -0700

    add cancel preview

--- src/components/LoginPage/Cancel.js
+++ src/components/LoginPage/Cancel.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import cancelImg from '../../img/login/cancel.png';
 
 function Cancel(){
+	const cta={
+		padding:'20px',
+		display:'block',
+		width:'40vw',
+		margin:'0 auto',
+		cursor:'pointer'
+	}
 	return (
 		<div>
-			<h1> Cancel </h1>
+			<h2>If you decide Netflix isn't for you - no problem. No commitment. Cancel online anytime.</h2>
+			<a style={cta} className="button caps">watch free for 30 days</a>
+			<img style={{width:'50vw'}} src={cancelImg} />
 		</div>
 		)
 };
